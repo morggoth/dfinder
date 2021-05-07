6f5ef71635ffe586a6a6ac241d8a898efde8ba0d
Konstantin Yakovlev <morggoth70@gmail.com>
2021-05-08 02:13:19 +0300

    Cleanup main.go

--- main.go
+++ main.go
@@ -59,35 +59,6 @@ func check_error(e error) {
 	}
 }
 
-// func hashWalk(path string, info os.FileInfo, err error) error {
-// 	if info.IsDir() {
-// 		log.Printf("%s\n", info.Name())
-// 	} else {
-// 		dat, err := ioutil.ReadFile(path)
-// 		if err != nil {
-// 			return err
-// 		}
-
-// 		sha256Hash := sha256.New()
-
-// 		_, err = io.Copy(sha256Hash, bytes.NewReader(dat))
-// 		if err != nil {
-// 			return err
-// 		}
-
-// 		sum := sha256Hash.Sum(nil)
-
-// 		absPath, err := filepath.Abs(path)
-// 		if err != nil {
-// 			return err
-// 		}
-
-// 		log.Printf("%x	%s\n", sum, absPath)
-// 	}
-
-// 	return nil
-// }
-
 func main() {
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
@@ -130,29 +101,4 @@ func main() {
 
 	err = filepath.Walk(".", walk)
 	check_error(err)
-
-	// hash := "3e90dcae65ccd49e160b02a5dd83f649c6be27bc9765cd038a9d1d2547a179fb"
-	// path := "/Users/morggoth/wk/p/dfinder/tests/MD5SUMS"
-
-	// dbInteract(hash, path, db)
-
-	// hash1 := "another_hash"
-	// path1 := "another_path"
-	// dbInteract(hash1, path1, db)
-
-	// dat, err := ioutil.ReadFile("tests/groovy-server-cloudimg-armhf-root.tar.xz")
-	// check_error(err)
-
-	// // md5Hash := md5.New()
-	// sha256Hash := sha256.New()
-
-	// _, err = io.Copy(sha256Hash, bytes.NewReader(dat))
-	// check_error(err)
-
-	// sum := sha256Hash.Sum(nil)
-
-	// fmt.Printf("%x\n", sum)
-	// // sha1Hash := sha1.New()
-	// // sha256Hash := sha256.New()
-
 }
